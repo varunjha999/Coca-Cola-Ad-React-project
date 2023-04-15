42711df7fa1b9b4b5900543d35a4186753a48276
Varun jha <jhavarun999@gmail.com>
2023-04-15 13:01:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import React,{useState} from 'react';
+import logoUrl from './coca-cola.gif';
+
+const MyFunction = () => {
+  window.open("https://www.coca-colacompany.com/");
+}
+
 
 function App() {
+
+  // const [isSpinning, setIsSpinning] = useState(false);
+
+  // const handleClick = () => {
+  //   setIsSpinning(!isSpinning);
+  // };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <>
+     <div  className="parent">
+        <h1> Wish You a Happy New Year </h1>
+
+        <div className="child">
+        
+        <button onClick={MyFunction}>Know More</button>
+
+        {/* <a href="https://www.coca-colacompany.com/">Know More</a>; */}
+
+        </div>
+
+        <div  className="child1"
+        
+        // {`child1 ${isSpinning ? "spinning" : ""}`}
+        //    onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+        {/* <img src="https://www.coca-colacompany.com/content/dam/journey/us/en/private/2020/12/2020-12-31-ccs-2020-year-in-review-1.jpg" alt="Happy New Year" /> */}
+         
+          <img src={logoUrl} alt="coca-cola-logo" width={200}  height={100}/>
+        </div>
+
+
+     </div>
+      </>
     </div>
   );
 }
